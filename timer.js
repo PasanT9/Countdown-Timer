5d10b5316e414a0773505f581d065afc304908e5
PasanT9 <pasan96tennakoon@gmail.com>
2019-09-12 16:47:51 +0530

    Updates: Sound Effects

--- timer.js
+++ timer.js
@@ -4,6 +4,7 @@ var sessionState=0;
 function startTimer(duration, timerDisplay) {
   var timer = duration, minutes, seconds;
   var timeQuery = document.querySelector('#timer');
+
   count = setInterval(function () {
     if(countState){
       minutes = parseInt(timer / 60, 10);
@@ -15,7 +16,7 @@ function startTimer(duration, timerDisplay) {
       timerDisplay.textContent = minutes + ":" +seconds;
 
       if(timer == 59 ){
-        console.log("Im here");
+        playSingleBell();
         timeQuery.style.color="#FF0D0D";
         timeQuery.style.textShadow="4px 4px white";
       }
@@ -23,7 +24,7 @@ function startTimer(duration, timerDisplay) {
 
       if (--timer < 0) {
         //timer = duration;
-        playAudio();
+        playDoubleBell();
         clearInterval(count);
         setSession(2);
       }
@@ -42,7 +43,7 @@ function setSession(state) {
   var timeQuery = document.querySelector('#timer');
   var titleQuery = document.querySelector('#title');
   if(state==0){
-    titleQuery.textContent = "PRESENTATION";
+    titleQuery.textContent = "IDEA PITCH";
     timeQuery.textContent = "00:00";
     sessionState=0;
     timeQuery.style.color = "white"
@@ -72,17 +73,18 @@ function pauseTime(){
   }
 }
 
-function playAudio() {
-  var audio = new Audio('service-bell_daniel_simion.ogg');
+function playSingleBell() {
+  var audio = new Audio('SingleBell.ogg');
   audio.type = 'audio/ogg';
 
-  var playPromise = audio.play();
+  audio.play();
 
-  if (playPromise !== undefined) {
-    playPromise.then(function () {
-      console.log('Playing....');
-    }).catch(function (error) {
-      console.log('Failed to play....' + error);
-    });
-  }
+}
+
+function playDoubleBell() {
+  var audio1 = new Audio('DoubleBell.ogg');
+  audio1.type = 'audio/ogg';
+
+  audio1.play();
+
 }
