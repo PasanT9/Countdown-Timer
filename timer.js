fe24d9458f68046165500ee58390d5b05bba972f
PasanT9 <pasan96tennakoon@gmail.com>
2019-09-12 08:41:47 +0530

    Finalized

--- timer.js
+++ timer.js
@@ -2,73 +2,56 @@ var count;
 var countState=true;
 var sessionState=0;
 function startTimer(duration, timerDisplay) {
-    var timer = duration, minutes, seconds;
-    var timeQuery = document.querySelector('#timer');
-    count = setInterval(function () {
-      if(countState){
-        timeQuery.style.fontFamily = "digital-7 mono";
-        timeQuery.style.fontSize = "300px";
-        minutes = parseInt(timer / 60, 10);
-        seconds = parseInt(timer % 60, 10);
+  var timer = duration, minutes, seconds;
+  var timeQuery = document.querySelector('#timer');
+  count = setInterval(function () {
+    if(countState){
+      minutes = parseInt(timer / 60, 10);
+      seconds = parseInt(timer % 60, 10);
 
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
+      minutes = minutes < 10 ? "0" + minutes : minutes;
+      seconds = seconds < 10 ? "0" + seconds : seconds;
 
-        timerDisplay.textContent = minutes + ":" +seconds;
+      timerDisplay.textContent = minutes + ":" +seconds;
 
-        if(sessionState == 0 && timer == 60 ){
-          playAudio();
-          timeQuery.style.color="#FF0D0D";
-          timeQuery.style.textShadow="4px 4px white";
-        }
-        if(sessionState == 1 && timer == 30 ){
-          playAudio();
-          timeQuery.style.color="#FF0D0D";
-          timeQuery.style.textShadow="4px 4px white";
-        }
-        if(sessionState == 1 && timer == 0){
-          clearInterval(count);
-          console.log("1 minute");
-          setSession(2);
-          playAudio();
-          countDown(1);
-        }
-        else if(sessionState == 2 && timer == 0){
-          clearInterval(count);
-          setSession(0);
-        }
-
-        else if (--timer < 0) {
-            //timer = duration;
-            console.log("Before");
-            clearInterval(count);
-            setSession(1);
-            console.log("After");
-        }
+      if(timer == 59 ){
+        console.log("Im here");
+        timeQuery.style.color="#FF0D0D";
+        timeQuery.style.textShadow="4px 4px white";
       }
-    }, 1000);
+
+
+      if (--timer < 0) {
+        //timer = duration;
+        playAudio();
+        clearInterval(count);
+        setSession(2);
+      }
+    }
+  }, 1000);
 }
 
 function countDown(time) {
-    clearInterval(count);
-    var fiveMinutes = 60 * time;
-    var timer = document.querySelector('#timer');
-    startTimer(fiveMinutes,timer);
+  clearInterval(count);
+  var fiveMinutes = 60 * time;
+  var timer = document.querySelector('#timer');
+  startTimer(fiveMinutes,timer);
 };
 
 function setSession(state) {
   var timeQuery = document.querySelector('#timer');
+  var titleQuery = document.querySelector('#title');
   if(state==0){
-      timeQuery.textContent = "00:00";
-      sessionState=0;
-      timeQuery.style.color = "white"
-      timeQuery.style.textShadow = "4px 4px black";
+    titleQuery.textContent = "PRESENTATION";
+    timeQuery.textContent = "00:00";
+    sessionState=0;
+    timeQuery.style.color = "white"
+    timeQuery.style.textShadow = "4px 4px black";
   }
   else if(state==1){
-    timeQuery.style.fontFamily = "sans-serif"
-    timeQuery.style.fontSize = "200px";
-    timeQuery.textContent = "Q & A";
     sessionState=1;
+    titleQuery.textContent = "Q & A";
+    timeQuery.textContent = "00:00";
     timeQuery.style.color = "white"
     timeQuery.style.textShadow = "4px 4px black";
   }
@@ -96,10 +79,10 @@ function playAudio() {
   var playPromise = audio.play();
 
   if (playPromise !== undefined) {
-      playPromise.then(function () {
-          console.log('Playing....');
-      }).catch(function (error) {
-          console.log('Failed to play....' + error);
-      });
+    playPromise.then(function () {
+      console.log('Playing....');
+    }).catch(function (error) {
+      console.log('Failed to play....' + error);
+    });
   }
 }
